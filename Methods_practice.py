40db5be2be4fa1be28232406ada8a70d7eecd4d8
mac <singh.ravneet1995@gmail.com>
2024-07-24 18:26:10 +0530

    Methods file added

---
+++ Methods_practice.py
@@ -0,0 +1,16 @@
+
+class Vector2D:
+    x = 0.0
+    y = 0.0
+
+    def Set(self,x,y):
+        self.x = x 
+        self.y = y
+
+def Main():
+    vec = Vector2D()
+    vec.Set(5,6)
+    print("X: " + str(vec.x) + ", Y: " + str(vec.y))
+
+if __name__ == '__main__':
+    Main()
