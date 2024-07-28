be0e7746be34eeaed92a9c15eed1d0a970c4c739
mac <singh.ravneet1995@gmail.com>
2024-07-28 18:00:32 +0530

    File updated

---
+++ DSA/Queue/queue_using_stack.py
@@ -0,0 +1,14 @@
+'''
+enQueue(q, x): 
+
+While stack1 is not empty, push everything from stack1 to stack2.
+Push x to stack1 (assuming size of stacks is unlimited).
+Push everything back to stack1.
+Here time complexity will be O(n)
+
+deQueue(q): 
+
+If stack1 is empty then error
+Pop an item from stack1 and return it
+Here time complexity will be O(1)
+'''
