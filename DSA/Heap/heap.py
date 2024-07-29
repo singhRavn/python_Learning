435603eaac430a24b9f8d14523e82951477f0ecc
mac <singh.ravneet1995@gmail.com>
2024-07-29 15:55:28 +0530

    Heap

---
+++ DSA/Heap/heap.py
@@ -0,0 +1,5 @@
+'''
+provides the heap data structure that is mainly used to represent a priority queue.
+The proprty of this data structure is that it always give the smallest element whenever the element is popped.
+
+'''
