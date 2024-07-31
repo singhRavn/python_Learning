a67407866207aaea513c8c6df53710bed65aade3
mac <singh.ravneet1995@gmail.com>
2024-07-31 12:52:02 +0530

    FActory Methoda

--- DesignPatters/factory_method.py
+++ DesignPatters/factory_method.py
@@ -6,3 +6,51 @@ the same common interface.
 
 
 '''
+
+
+# Python Code for factory method 
+# it comes under the creational 
+# Design Pattern
+
+class FrenchLocalizer:
+    def __init__(self):        
+        self.translations = {"car": "voiture", "bike": "bicyclette",
+                             "cycle":"cyclette"}
+
+    def localize(self, msg):
+        return self.translations.get(msg, msg)
+
+class SpanishLocalizer:
+
+    def __init__(self):
+        self.translations = {"car": "coche", "bike": "bicicleta",
+                             "cycle":"ciclo"}
+
+    def localize(self, msg):
+        return self.translations.get(msg, msg)
+
+class EnglishLocalizer:
+    def localize(self, msg):
+        return msg
+
+def Factory(language ="English"):
+    localizers = {
+        "French": FrenchLocalizer,
+        "English": EnglishLocalizer,
+        "Spanish": SpanishLocalizer,
+    }
+
+    return localizers[language]()
+
+if __name__ == "__main__":
+
+    f = Factory("French")
+    e = Factory("English")
+    s = Factory("Spanish")
+
+    message = ["car", "bike", "cycle"]
+
+    for msg in message:
+        print(f.localize(msg))
+        print(e.localize(msg))
+        print(s.localize(msg))
