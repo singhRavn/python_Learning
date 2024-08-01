6c5959b76bfead988bf21315e7c140bc81765985
mac <singh.ravneet1995@gmail.com>
2024-08-01 19:50:06 +0530

    factory methods update

--- DesignPatters/factory_method.py
+++ DesignPatters/factory_method.py
@@ -3,8 +3,6 @@ this is creational design pattern that allows an interface or a class to create 
 but let subclasses decide ehich class or objecy to instantiate.
 Here, objects are created exposing the logic to client and for creating the new type of object, the client uses
 the same common interface.
-
-
 '''
 
 
