898c2f0ef55fd297d5322c9e7737ff3b68d4d92c
mac <singh.ravneet1995@gmail.com>
2024-08-02 20:47:23 +0530

    No value

--- DesignPatters/factory_method.py
+++ DesignPatters/factory_method.py
@@ -2,7 +2,8 @@
 this is creational design pattern that allows an interface or a class to create an object,
 but let subclasses decide ehich class or objecy to instantiate.
 Here, objects are created exposing the logic to client and for creating the new type of object, the client uses
-the same common interface.
+the same common interface. 
+
 '''
 
 
