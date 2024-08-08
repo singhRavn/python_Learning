dc0c6767c15179d27328fc8838a680e1b46f379b
mac <singh.ravneet1995@gmail.com>
2024-08-08 19:37:38 +0530

    Btree

---
+++ DSA/Tree/BinaryTree/binarytree.py
@@ -0,0 +1,6 @@
+'''
+A Binary Tree Data Structure is a hierarchical data structure in which each node has at most two children,
+referred to as the left child and the right child.
+It is commonly used in computer science for efficient storage and retrieval of data,
+with various operations such as insertion, deletion, and traversal.
+'''
