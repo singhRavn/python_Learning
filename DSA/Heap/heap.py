cd940b32fa2f4a44655f1f899cbd95e9d3a68c85
singhRavn <singh.ravneet1995@gmail.com>
2024-08-20 20:47:52 +0530

    heap

--- DSA/Heap/heap.py
+++ DSA/Heap/heap.py
@@ -3,3 +3,18 @@ provides the heap data structure that is mainly used to represent a priority que
 The proprty of this data structure is that it always give the smallest element whenever the element is popped.
 
 '''
+import heapq
+
+li = [5, 7, 9, 1, 3]
+heapq.heapify(li)
+
+print("The created heap is : ", end="")
+print(list(li))
+
+heapq.heappush(li, 4)
+
+print("The modified heap after push is : ", end="")
+print(list(li))
+
+print("The popped and smallest element is : ", end="")
+print(heapq.heappop(li))
