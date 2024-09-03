7c623d08906039a1b0f1a06295cc9c893968fc38
singhRavn <singh.ravneet1995@gmail.com>
2024-09-03 19:52:30 +0530

    "Vector datbase"

---
+++ TryAI/vectordb.py
@@ -0,0 +1,8 @@
+'''
+Vector databases store data by using vector embeddings.
+Vector embeddings in vector databases refer to a way of representing objects,
+such as items, documents, or data points, as vectors in a multi-dimensional space.
+Each object is assigned a vector that captures various characteristics or features of that object. 
+These vectors are designed in such a way that similar objects have vectors that are closer
+to each other in the vector space, while dissimilar objects have vectors that are farther apart.
+'''
